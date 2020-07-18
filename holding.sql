a89d1401fb9c85d15bdd47e21e03cc5a25289f6b
joeylam <joey.cm.lam@gmail.com>
2020-07-18 21:29:38 +0800

    update create table

---
+++ holding.sql
@@ -0,0 +1,10 @@
+insert into holding (stock_code, quantity, last_udpate) values ('0002.HK', 2000.0, CURRENT_TIMESTAMP);
+insert into holding (stock_code, quantity, last_udpate) values ('0005.HK', 400.0, CURRENT_TIMESTAMP);
+insert into holding (stock_code, quantity, last_udpate) values ('0011.HK', 1500.0, CURRENT_TIMESTAMP);
+insert into holding (stock_code, quantity, last_udpate) values ('0066.HK', 4000.0, CURRENT_TIMESTAMP);
+insert into holding (stock_code, quantity, last_udpate) values ('0405.HK', 13000.0, CURRENT_TIMESTAMP);
+insert into holding (stock_code, quantity, last_udpate) values ('0823.HK', 8500.0, CURRENT_TIMESTAMP);
+insert into holding (stock_code, quantity, last_udpate) values ('1299.HK', 5000.0, CURRENT_TIMESTAMP);
+insert into holding (stock_code, quantity, last_udpate) values ('2800.HK', 3000.0, CURRENT_TIMESTAMP);
+insert into holding (stock_code, quantity, last_udpate) values ('0778.HK', 15000.0, CURRENT_TIMESTAMP);
+
